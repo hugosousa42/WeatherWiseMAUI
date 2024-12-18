a9047743aa691fe5fd6aebf1ebafaa0928c1ac11
hugosousa42 <hugosousa42@proton.me>
2024-12-18 02:35:19 +0000

    OpenWeather API.

---
+++ WeatherWiseMAUI/Models/Wind.cs
@@ -0,0 +1,9 @@
+namespace WeatherWiseMAUI.Models{ 
+
+    public class Wind
+    {
+        public double speed { get; set; }
+        public int deg { get; set; }
+    }
+
+}
---
+++ WeatherWiseMAUI/Services/ApiWeatherService.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using WeatherWiseMAUI.Models;
+
+namespace WeatherWiseMAUI.Services
+{
+    public class ApiWeatherService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _apiKey = "4c324ec18c2d583aa51ffcedd83c981e"; 
+        private readonly ILogger<ApiWeatherService> _logger;
+
+        JsonSerializerOptions _serializerOptions;
+
+        public ApiWeatherService(HttpClient httpClient, ILogger<ApiWeatherService> logger)
+        {
+            _httpClient = httpClient;
+            _logger = logger;
+            _serializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+
+        public async Task<WeatherData> GetWeatherAsync(string city)
+        {
+            var url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={_apiKey}&units=metric";
+            try
+            {
+                var response = await _httpClient.GetStringAsync(url);
+                return JsonSerializer.Deserialize<WeatherData>(response, _serializerOptions);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Erro ao obter dados meteorológicos: {ex.Message}");
+                return null;
+            }
+        }
+    }
+}
